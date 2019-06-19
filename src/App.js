7d9adf52930d9731ec12110afb9954ee7d29a897
dkang417 <dkang417@gmail.com>
2019-06-18 22:33:13 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-const Pet = ({name, animal, breed}) => {
-    return React.createElement("div", {}, [
-        React.createElement("h1", {}, name),
-        React.createElement("h2", {}, animal),
-        React.createElement("h2", {}, breed)
-    ]);
-};
+import React from 'react';
+import { render } from 'react-dom';
+import SearchParams from './SearchParams';
+
 
 const App = () => {
-    return React.createElement(
-        "div",
-        {},
-        [
-            React.createElement("h1", {}, "Adopt Me!"),
-            React.createElement(Pet, { name: "luna", animal: "dog", breed: "poodle"}),    
-            React.createElement(Pet, { name: "Pepper", animal: "bird", breed: "parrot"}), 
-            React.createElement(Pet, { name: "Doink", animal: "cat", breed: "mix"}), 
+    // return React.createElement(
+    //     "div",
+    //     {},
+    //     [
+    //         React.createElement("h1", {}, "Adopt Me!"),
+    //         React.createElement(Pet, { name: "luna", animal: "dog", breed: "poodle"}),    
+    //         React.createElement(Pet, { name: "Pepper", animal: "bird", breed: "parrot"}), 
+    //         React.createElement(Pet, { name: "Doink", animal: "cat", breed: "mix"}), 
         
-        ]);
+    //     ]);
+    return (
+        <div>
+            <h1> Adopt me! </h1>
+            <SearchParams />
+        </div>
+    );
 };
 
-ReactDOM.render(
-    React.createElement(App), 
-    document.getElementById("root")
-);
+render(<App /> , document.getElementById("root"));
